6b027fe37fdaf967f0905a1bb122408e768915a0
Matthew Spellings <mspells@umich.edu>
2019-11-12 09:23:46 -0800

    Improve control visibility effects

--- dnd_aoe.js
+++ dnd_aoe.js
@@ -11,6 +11,13 @@ var render_scene = function()
     let feet_per_square = 5
     let translucent_color = 'rgba(50, 70, 200, 0.5)'
     let line_props = {strokeWidth: 3, stroke: 'black', selectable: false, evented: false}
+    let group_props = {centeredRotation: false, originY: 'center',
+                       lockScalingX: true, lockScalingY: true,
+                       hasBorders: false, cornerSize: pixel_scale/5,
+                       transparentCorners: false,
+                       cornerColor: 'rgba(25, 125, 35, 0.5)'}
+    let control_viz = {bl: false, br: false, mb: false, ml: false,
+                       mr: false, mt: false, tl: false, tr: false}
 
     let draw_lines = function() {
         let grid_lines = []
@@ -63,8 +70,10 @@ var render_scene = function()
             selectable: false, evented: false,
         })
 
-        let group = new fabric.Group([large_circle, get_center_circle()],
-                                     {lockScalingX: true, lockScalingY: true})
+        let group = new fabric.Group(
+            [large_circle, get_center_circle()],
+            group_props)
+        group.setControlsVisibility(control_viz)
         canvas.add(group)
     }
     sphere_button.addEventListener('click', draw_sphere)
@@ -92,9 +101,10 @@ var render_scene = function()
         let dot = get_center_circle()
         let group = new fabric.Group(
             [cone, dot],
-            {centeredRotation: false, originY: 'center',
-             originX: dot.radius/(dot.radius + radius),
-             lockScalingX: true, lockScalingY: true})
+            Object.assign(
+                {originX: dot.radius/(dot.radius + radius)},
+                group_props))
+        group.setControlsVisibility(control_viz)
         canvas.add(group).setActiveObject(group)
     }
     cone_button.addEventListener('click', draw_cone)
@@ -124,9 +134,10 @@ var render_scene = function()
         let dot = get_center_circle()
         let group = new fabric.Group(
             [line, dot],
-            {centeredRotation: false, originY: 'center',
-             originX: dot.radius/(dot.radius + length),
-             lockScalingX: true, lockScalingY: true})
+            Object.assign(
+                {originX: dot.radius/(dot.radius + length)},
+                group_props))
+        group.setControlsVisibility(control_viz)
         canvas.add(group).setActiveObject(group)
     }
     line_button.addEventListener('click', draw_line)
